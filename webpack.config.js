368df6f5f9ee7fd0b28d49dd272846a3502f07d5
Florian Körner <f.koerner@checkdomain.de>
2018-01-28 19:31:45 +0100

    Add: License informations

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,64 @@
+const licenseChecker = require('license-checker');
 const nodeExternals = require('webpack-node-externals');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const path = require('path');
 
-let baseConfig = {
-  entry: './lib/index.js',
-  devtool: 'sourcemap',
-  target: 'web',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'avatars.js',
-    library: 'Avatars',
-    libraryTarget: 'umd',
-    libraryExport: 'default',
-    umdNamedDefine: true
-  }
+module.exports = async () => {
+  let licenseInformations = await new Promise((resolve, reject) => {
+    licenseChecker.init(
+      {
+        start: __dirname,
+        production: true
+      },
+      function(err, json) {
+        err ? reject(err) : resolve(json);
+      }
+    );
+  });
+
+  let licenses = [];
+
+  Object.keys(licenseInformations).forEach(name => {
+    let license = licenseInformations[name];
+
+    licenses.push(
+      [
+        name + ':',
+        '  author: ' + license.publisher,
+        '  license: ' + license.licences,
+        '  repository: ' + license.repository
+      ].join('\n')
+    );
+  });
+
+  let banner = licenses.join('\n\n');
+
+  let baseConfig = {
+    entry: './lib/index.js',
+    devtool: 'sourcemap',
+    target: 'web',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'avatars.js',
+      library: 'Avatars',
+      libraryTarget: 'umd',
+      libraryExport: 'default',
+      umdNamedDefine: true
+    },
+    plugins: [new webpack.BannerPlugin(banner)]
+  };
+
+  let minifyConfig = merge({}, baseConfig, {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true
+      })
+    ],
+    output: {
+      filename: 'avatars.min.js'
+    }
+  });
+
+  return [baseConfig, minifyConfig];
 };
-
-let minifyConfig = merge({}, baseConfig, {
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true
-    })
-  ],
-  output: {
-    filename: 'avatars.min.js'
-  }
-});
-
-module.exports = [baseConfig, minifyConfig];
