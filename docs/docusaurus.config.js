4e48ef05ee2c0abbd6c69c206b8b0533fa504d3b
Florian Körner <contact@florian-koerner.com>
2020-11-18 21:05:05 +0100

    Revert "Add: FAQ"
    This reverts commit 88550263a7868840deec199f03a8985c1ad6d905.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -29,12 +29,6 @@ module.exports = {
           position: 'left',
         },
         {
-          to: 'faq/',
-          activeBasePath: 'faq',
-          label: 'FAQ',
-          position: 'left',
-        },
-        {
           href: 'https://github.com/DiceBear/avatars',
           label: 'GitHub',
           position: 'right',
