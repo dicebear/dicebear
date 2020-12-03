ba4231af9296772202dcbb9f9a8e54b3a19e907b
Florian Körner <contact@florian-koerner.com>
2020-12-03 21:54:50 +0100

    Change: Move serverless to separate repository

--- serverless/src/options/gridy.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-import defaults from './defaults';
-
-export default yup.object({
-  ...defaults,
-  colorful: yup.number().oneOf([0, 1]),
-  deterministic: yup.number().oneOf([0, 1]),
-});
--- serverless/src/options/male.ts
+++
@@ -1,7 +0,0 @@
-import * as yup from 'yup';
-import defaults from './defaults';
-
-export default yup.object({
-  ...defaults,
-  mood: yup.array().of(yup.string().oneOf(['happy', 'sad', 'surprised'])),
-});
