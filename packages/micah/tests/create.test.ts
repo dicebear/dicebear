1cbf599a8163842a905418742c066c16b13f7c89
Florian Körner <contact@florian-koerner.com>
2021-04-11 22:48:40 +0200

    Fix: Micah test

--- packages/micah/tests/create.test.ts
+++ packages/micah/tests/create.test.ts
@@ -1,6 +1,10 @@
-import { default as Avatars, createAvatar } from '@dicebear/avatars';
-import legacyStyle from '../lib';
+import * as avatars from '@dicebear/avatars';
+import * as style from '../lib';
 
-test('avataaars.create.legacy', () => {
-    expect(new Avatars(legacyStyle).create()).toContain('svg');
+test('micah.create', () => {
+    expect(avatars.createAvatar(style)).toContain('svg');
+});
+
+test('micah.create.legacy', () => {
+    expect(new avatars.default(style.default).create()).toContain('svg');
 });
