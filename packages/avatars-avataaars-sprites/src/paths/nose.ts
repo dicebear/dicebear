f31f1e47aa8149726edc67cf63c64e0af4b06f8a
Florian Körner <contact@florian-koerner.com>
2021-04-15 21:15:01 +0200

    Fix: build

--- packages/avatars-avataaars-sprites/src/paths/nose.ts
+++ packages/avatars-avataaars-sprites/src/paths/nose.ts
@@ -1,4 +1,4 @@
-export const nose: Record<'default', (color: string) => string> = {
+export const nose: Record<'default', () => string> = {
   default: () => `
     <path fill-rule="evenodd" clip-rule="evenodd" d="M16 8c0 4.418 5.373 8 12 8s12-3.582 12-8" fill="#000" fill-opacity=".16"/>
   `,
