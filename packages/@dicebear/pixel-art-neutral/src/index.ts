94456568187b1a448c241201e892060b5baabd2d
Florian Körner <contact@florian-koerner.com>
2021-09-09 10:58:00 +0200

    FIx: License Files

--- packages/@dicebear/pixel-art-neutral/src/index.ts
+++ packages/@dicebear/pixel-art-neutral/src/index.ts
@@ -4,7 +4,7 @@
  * Code licensed under MIT License.
  * Copyright (c) 2021 Florian Körner
  *
- * Design "Pixel Art - Neutral" by Plastic Jam licensed under MIT.
+ * Design "8biticon" by Plastic Jam licensed under MIT.
  * License: https://github.com/dicebear/dicebear/blob/main/packages/%40dicebear/pixel-art-neutral/LICENSE
  */
 
