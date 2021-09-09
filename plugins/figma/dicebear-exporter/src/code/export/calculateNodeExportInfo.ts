e16a26c7055f6bd0bde15cb2b0b169af8c864c5e
Florian Körner <contact@florian-koerner.com>
2021-09-09 08:03:27 +0200

    FIx: clip only on frame

--- plugins/figma/dicebear-exporter/src/code/export/calculateNodeExportInfo.ts
+++ plugins/figma/dicebear-exporter/src/code/export/calculateNodeExportInfo.ts
@@ -15,8 +15,10 @@ export async function calculateNodeExportInfo(node: ComponentNode | FrameNode) {
   const nodeClone = node.clone();
 
   try {
-    // For the export, clip-path must be set in Figma so that the viewport has the correct height and width.
-    nodeClone.clipsContent = true;
+    if (nodeClone.type === 'FRAME') {
+      // For the export, clip-path must be set in Figma so that the viewport has the correct height and width.
+      nodeClone.clipsContent = true;
+    }
 
     for (const instanceNode of findAllInstanceNodes(nodeClone)) {
       const mainComponent = instanceNode.mainComponent!;
