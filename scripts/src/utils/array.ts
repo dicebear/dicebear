5612ea98036900cc8796fcfcbfde8fd46138a569
Florian Körner <contact@florian-koerner.com>
2021-09-11 22:17:59 +0200

    Remove: Outdated scripts and website

--- scripts/src/utils/array.ts
+++
@@ -1,9 +0,0 @@
-export function chunk<T>(arr: T[], size: number): T[][] {
-  let result: T[][] = [];
-
-  do {
-    result.push(arr.splice(0, size));
-  } while (arr.length > 0);
-
-  return result;
-}
