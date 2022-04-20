8e7cafa7ba36a3d6d3a6b01b11cadcc306e8309f
Florian Körner <contact@florian-koerner.com>
2022-04-20 23:17:39 +0200

    Add: @dicebear/converter tests

---
+++ packages/@dicebear/converter/tests/png.test.js
@@ -0,0 +1,20 @@
+import { toFormat } from '../lib/node/index.js';
+import * as fs from 'fs';
+import * as path from 'path';
+import { test } from 'uvu';
+import { not } from 'uvu/assert';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const avatar = fs.readFileSync(path.resolve(__dirname, 'avatar.svg'), {
+  encoding: 'utf8',
+});
+
+test(`Convert to png buffer`, async () => {
+  not.throws(() => toFormat(avatar, 'png').toArrayBuffer());
+});
+
+test(`Convert to png data uri`, async () => {
+  not.throws(() => toFormat(avatar, 'png').toDataUri());
+});
+
+test.run();
---
+++ packages/@dicebear/converter/tests/svg.test.js
@@ -0,0 +1,20 @@
+import { toFormat } from '../lib/node/index.js';
+import * as fs from 'fs';
+import * as path from 'path';
+import { test } from 'uvu';
+import { not } from 'uvu/assert';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const avatar = fs.readFileSync(path.resolve(__dirname, 'avatar.svg'), {
+  encoding: 'utf8',
+});
+
+test(`Convert to svg buffer`, async () => {
+  not.throws(() => toFormat(avatar, 'svg').toArrayBuffer());
+});
+
+test(`Convert to svg data uri`, async () => {
+  not.throws(() => toFormat(avatar, 'svg').toDataUri());
+});
+
+test.run();
