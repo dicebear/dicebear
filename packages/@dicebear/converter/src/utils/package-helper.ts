b0c382e1ea5e58236d7bda510521fcf3952e3b93
Florian Körner <contact@florian-koerner.com>
2022-04-26 22:30:20 +0200

    Add: Converter optional peer dependencies

--- packages/@dicebear/converter/src/utils/package-helper.ts
+++ packages/@dicebear/converter/src/utils/package-helper.ts
@@ -3,24 +3,10 @@ import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 
 export async function ensurePackage(name: string, version: string) {
-  let packageJson: { version: string };
-
   try {
-    packageJson = require(`${name}/package.json`);
+    require.resolve(name);
   } catch (e) {
     // prettier-ignore
     throw new Error(`Please install \`${name}@^${version}\` to use this feature.'`);
-  }
-
-  const packageVersion = packageJson.version.split('.').map((v) => parseInt(v));
-  const requiredVersion = version.split('.').map((v) => parseInt(v));
-
-  if (
-    packageVersion[0] !== requiredVersion[0] ||
-    packageVersion[1] < requiredVersion[1]
-  ) {
-    throw new Error(
-      `Please install \`${name}@^${version}\` to use this feature. (Currently installed version: \`${packageJson.version})\`})'`
-    );
   }
 }
