0fbc92f9d17eb5118214d51059623e14b8dcfc70
Florian Körner <contact@florian-koerner.com>
2022-06-16 20:01:59 +0200

    Change: Simplify schema

--- packages/@dicebear/initials/src/schema.ts
+++ packages/@dicebear/initials/src/schema.ts
@@ -1,15 +1,13 @@
 import type { StyleSchema } from '@dicebear/core';
 
 export const schema: StyleSchema = {
-  title: 'Options',
   $schema: 'http://json-schema.org/draft-07/schema#',
   properties: {
     backgroundColor: {
       type: 'array',
       items: {
         type: 'string',
-        pattern:
-          '^#([a-fA-F0-9]{3}|[a-fA-F0-9]{4}|[a-fA-F0-9]{6}|[a-fA-F0-9]{8})$',
+        pattern: '^[a-fA-F0-9]{6}$',
       },
       default: [
         'e53935',
@@ -32,48 +30,42 @@ export const schema: StyleSchema = {
     },
     fontFamily: {
       type: 'array',
-      uniqueItems: true,
       minItems: 1,
       items: {
         type: 'string',
-        pattern: '^[a-zA-Z0-9\\-\\s]+$',
+        enum: [
+          'Arial',
+          'Verdana',
+          'Helvetica',
+          'Tahoma',
+          'Trebuchet MS',
+          'Times New Roman',
+          'Georgia',
+          'Garamond',
+          'Courier New',
+          'Brush Script MT',
+          'sans-serif',
+          'serif',
+        ],
       },
       default: ['Arial', 'sans-serif'],
-      examples: [
-        ['Arial'],
-        ['Verdana'],
-        ['Helvetica'],
-        ['Tahoma'],
-        ['Trebuchet MS'],
-        ['Times New Roman'],
-        ['Georgia'],
-        ['Garamond'],
-        ['Courier New'],
-        ['Brush Script MT'],
-        ['sans-serif'],
-        ['serif'],
-      ],
     },
     fontSize: {
       type: 'integer',
       minimum: 1,
       maximum: 100,
       default: 50,
-      examples: [25, 50, 75, 100],
     },
     chars: {
       type: 'number',
       minimum: 0,
       maximum: 2,
       default: 2,
-      examples: [1, 2],
     },
     fontWeight: {
       type: 'number',
       default: 400,
       enum: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-      examples: [100, 200, 300, 400, 500, 600, 700, 800, 900],
     },
   },
-  additionalProperties: false,
 };
