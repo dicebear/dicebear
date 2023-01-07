e84471735cd3afe5235d5e26dac281bb60a49271
Florian Körner <contact@florian-koerner.com>
2023-01-07 21:12:25 +0100

    test

--- packages/@dicebear/converter/src/index.ts
+++ packages/@dicebear/converter/src/index.ts
@@ -4,6 +4,7 @@
  * Code licensed under MIT (https://github.com/dicebear/dicebear/blob/main/LICENSE)
  * Copyright (c) 2022 Florian Körner
  */
-
+import { toFormat } from './core.js';
 export { toFormat } from './core.js';
 export * from './types.js';
+export default { toFormat };
