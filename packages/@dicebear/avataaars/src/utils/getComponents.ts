6279b3eea85b03f812ed904d25a32b403665eb6f
Florian Körner <contact@florian-koerner.com>
2023-03-16 19:52:12 +0100

    Fix #287: Avataaars mask

--- packages/@dicebear/avataaars/src/utils/getComponents.ts
+++ packages/@dicebear/avataaars/src/utils/getComponents.ts
@@ -18,52 +18,52 @@ export function getComponents({ prng, options }: Props): ComponentPickCollection
   const styleComponent = pickComponent({
     prng,
     group: 'style',
-    values: options.style
+    values: options.style,
   });
   const clothingComponent = pickComponent({
     prng,
     group: 'clothing',
-    values: options.clothing
+    values: options.clothing,
   });
   const mouthComponent = pickComponent({
     prng,
     group: 'mouth',
-    values: options.mouth
+    values: options.mouth,
   });
   const noseComponent = pickComponent({
     prng,
     group: 'nose',
-    values: options.nose
+    values: options.nose,
   });
   const eyesComponent = pickComponent({
     prng,
     group: 'eyes',
-    values: options.eyes
+    values: options.eyes,
   });
   const eyebrowsComponent = pickComponent({
     prng,
     group: 'eyebrows',
-    values: options.eyebrows
+    values: options.eyebrows,
   });
   const topComponent = pickComponent({
     prng,
     group: 'top',
-    values: options.top
+    values: options.top,
   });
   const facialHairComponent = pickComponent({
     prng,
     group: 'facialHair',
-    values: options.facialHair
+    values: options.facialHair,
   });
   const accessoriesComponent = pickComponent({
     prng,
     group: 'accessories',
-    values: options.accessories
+    values: options.accessories,
   });
   const clothingGraphicComponent = pickComponent({
     prng,
     group: 'clothingGraphic',
-    values: options.clothingGraphic
+    values: options.clothingGraphic,
   });
 
   return {
