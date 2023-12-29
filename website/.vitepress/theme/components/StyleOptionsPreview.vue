65bc4052abc4c0ed671f1a744a420eb5be43dc3a
Florian Körner <contact@florian-koerner.com>
2023-12-29 05:46:02 +0100

    Fix #395: Optimise copying of preview values

--- website/.vitepress/theme/components/StyleOptionsPreview.vue
+++ website/.vitepress/theme/components/StyleOptionsPreview.vue
@@ -4,46 +4,51 @@ import { getAvatarPropertyPreviewOptions } from '@shared/utils/avatar';
 import Avatar from '@shared/components/Avatar.vue';
 
 const props = defineProps({
-  styleName: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  value: {
-    required: true,
-  },
+    styleName: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    value: {
+        required: true,
+    },
 });
 
 const options = computed(() =>
-  getAvatarPropertyPreviewOptions(props.name, props.value)
+    getAvatarPropertyPreviewOptions(props.name, props.value)
 );
 </script>
 
 <template>
-  <div class="preview">
-    <Avatar
-      :size="name === 'size' ? parseInt(value as any) : 64"
-      :styleName="styleName"
-      :styleOptions="options"
-      class="preview-avatar"
-    />
-    <code>{{ value }}</code>
-  </div>
+    <div class="preview">
+        <Avatar
+            :size="name === 'size' ? parseInt(value as any) : 64"
+            :styleName="styleName"
+            :styleOptions="options"
+            class="preview-avatar"
+        />
+        <code>{{ value }}</code>
+    </div>
+    <div class="invisible-char">,</div>
 </template>
 
 <style scoped lang="scss">
 .preview {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin: 9px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 9px;
 
-  &-avatar {
-    margin-bottom: 12px;
-  }
+    &-avatar {
+        margin-bottom: 12px;
+        user-select: none;
+    }
+}
+.invisible-char {
+    opacity: 0;
 }
 </style>
