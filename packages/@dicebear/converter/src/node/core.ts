bed44831e3d841c3af1e5b4bbd50488936e4a8e3
Florian Körner <contact@florian-koerner.com>
2024-03-05 22:57:37 +0100

    Fix: Do not create exiftool *_original files

--- packages/@dicebear/converter/src/node/core.ts
+++ packages/@dicebear/converter/src/node/core.ts
@@ -105,10 +105,11 @@ async function toBuffer(
     );
     const exiftool = (await import('exiftool-vendored')).exiftool;
 
-    await tmp.withFile(async ({ path }) => {
+    buffer = await tmp.withFile(async ({ path }) => {
       await fs.writeFile(path, buffer);
-      await exiftool.write(path, exif);
-      buffer = await fs.readFile(path);
+      await exiftool.write(path, exif, ['-overwrite_original']);
+      
+      return fs.readFile(path);
     });
   }
 
