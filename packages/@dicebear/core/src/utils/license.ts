6d158d425765c7e733ae01249ffd4f46278d9cd1
Florian Körner <contact@florian-koerner.com>
2024-03-05 22:59:38 +0100

    Change: Copyright metadata standardized

--- packages/@dicebear/core/src/utils/license.ts
+++ packages/@dicebear/core/src/utils/license.ts
@@ -2,78 +2,89 @@ import { Exif, Style } from '../types.js';
 import * as _ from './escape.js';
 
 export function xml(style: Style<any>): string {
-  const title = style.meta?.title ?? 'Unnamed';
-  const creator = style.meta?.creator ?? 'Unknown';
+  const title = style.meta?.title;
+  const creator = style.meta?.creator;
+  const source = style.meta?.source;
+  const license = style.meta?.license?.url;
+  const rights = text(style);
 
-  let description = `"${title}" by "${creator}"`;
-
-  if (style.meta?.license?.name) {
-    description += `, licensed under "${style.meta.license.name}".`;
+  if (!title && !creator && !source && !license && !rights) {
+    return '';
   }
 
-  description += ' / Remix of the original. - Created with dicebear.com';
-
-  const xmlTitle = `<dc:title>${_.xml(title)}</dc:title>`;
-
-  const xmlCreator =
-    '<dc:creator>' +
-    `<cc:Agent rdf:about="${_.xml(style.meta?.homepage ?? '')}">` +
-    `<dc:title>${_.xml(creator)}</dc:title>` +
-    '</cc:Agent>' +
-    '</dc:creator>';
-
-  const xmlSource = style.meta?.source
-    ? `<dc:source>${_.xml(style.meta.source)}</dc:source>`
-    : '';
-
-  const xmlLicense = style.meta?.license?.url
-    ? `<cc:license rdf:resource="${_.xml(style.meta.license.url)}" />`
-    : '';
-
+  // https://nsteffel.github.io/dublin_core_generator/generator.html
   return (
-    `<desc>${description}</desc>` +
     '<metadata' +
+    ' xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"' +
+    ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
     ' xmlns:dc="http://purl.org/dc/elements/1.1/"' +
-    ' xmlns:cc="http://creativecommons.org/ns#"' +
-    ' xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">' +
+    ' xmlns:dcterms="http://purl.org/dc/terms/">' +
     '<rdf:RDF>' +
-    '<cc:Work>' +
-    xmlTitle +
-    xmlCreator +
-    xmlSource +
-    xmlLicense +
-    '</cc:Work>' +
+    '<rdf:Description>' +
+    (title ? `<dc:title>${_.xml(title)}</dc:title>` : '') +
+    (creator ? `<dc:creator>${_.xml(creator)}</dc:creator>` : '') +
+    (source ? `<dc:source xsi:type="dcterms:URI">${_.xml(style.meta?.source ?? '')}</dc:source>` : '') +
+    (license ? `<dcterms:license xsi:type="dcterms:URI">${_.xml(license)}</dcterms:license>` : '') +
+    (rights ? `<dc:rights>${_.xml(rights)}</dc:rights>` : '') +
+    '</rdf:Description>' +
     '</rdf:RDF>' +
     '</metadata>'
   );
 }
 
-export function exif(style: Style<any>): Exif {
-  const title = style.meta?.title ?? 'Unnamed';
-  const creator = style.meta?.creator ?? 'Unknown';
-
-  let copyright = `"${title}" by "${creator}"`;
-
-  if (style.meta?.license?.name) {
-    copyright += `, licensed under "${style.meta.license.name}".`;
-  }
-
-  copyright += ' / Remix of the original.';
-
-  const exif: Exif = {
-    ImageDescription: `${copyright} - Created with dicebear.com`,
-    Copyright: copyright,
-    'XMP-dc:Title': title,
-    'XMP-dc:Creator': creator,
-  };
+export function text(style: Style<any>): string {
+  let title = style.meta?.title ? `„${style.meta?.title}”` : 'Design';
+  let creator = `„${style.meta?.creator ?? 'Unknown'}”`;
 
   if (style.meta?.source) {
-    exif['XMP-dc:Source'] = style.meta.source;
+    title += ` (${style.meta.source})`;
   }
 
-  if (style.meta?.license?.url) {
-    exif['XMP-cc:License'] = style.meta.license.url;
+  let result = '';
+  
+  if (style.meta?.license?.name !== 'MIT' && style.meta?.title) {
+    result += 'Remix of ';
   }
 
-  return exif;
+  result += `${title} by ${creator}`;
+
+  if (style.meta?.license?.name) {
+    result += `, licensed under „${style.meta?.license?.name}”`;
+
+    if (style.meta?.license?.url) {
+      result += ` (${style.meta.license.url})`;
+    }
+  }
+  
+  return result;
+}
+
+export function exif(style: Style<any>): Exif {
+  const copyright = text(style);
+
+  // https://www.iptc.org/std/photometadata/specification/IPTC-PhotoMetadata
+  // https://developers.google.com/search/docs/appearance/structured-data/image-license-metadata
+  return {
+    // Title
+    'IPTC:ObjectName': style.meta?.title,
+    'XMP-dc:Title': style.meta?.title,
+
+    // Copyright
+    'IPTC:CopyrightNotice': copyright,
+    'XMP-dc:Rights': copyright,
+    
+    // Creator
+    'IPTC:By-line': style.meta?.creator,
+    'XMP-dc:Creator': style.meta?.creator,
+    
+    // Credit
+    'IPTC:Credit': style.meta?.creator,
+    'XMP-photoshop:Credit': style.meta?.creator,
+
+    // Licensor
+    'XMP-plus:LicensorURL': style.meta?.source,
+
+    // Rights
+    'XMP-xmpRights:WebStatement': style.meta?.license?.url,
+  } as Exif;
 }
