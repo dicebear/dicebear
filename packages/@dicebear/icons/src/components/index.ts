51b09070826d289d0e71d68b306e5a18a9426135
Florian Körner <647303+FlorianKoerner@users.noreply.github.com>
2024-03-16 11:48:55 +0100

    Remove: Style packages

--- packages/@dicebear/icons/src/components/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Do not change this file manually! This file was generated with the "Dicebear Exporter"-Plugin for Figma.
- *
- * Plugin: https://www.figma.com/community/plugin/1005765655729342787
- * File: https://www.figma.com/file/BRj9eonsORJ7GIUdm8gnu5
- */
-
-export { icon } from './icon.js';
--- packages/@dicebear/initials/src/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Options {
-    backgroundColor?: string[];
-    textColor?: string[];
-    fontFamily?: string[];
-    fontSize?: number;
-    chars?: number;
-}
