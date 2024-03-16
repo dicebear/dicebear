fa7e454920490c48288036c333e7605ebfab946c
Florian Körner <647303+FlorianKoerner@users.noreply.github.com>
2024-03-16 12:56:35 +0100

    Change: Copyright metadata standardized

--- packages/@dicebear/converter/src/node/core.ts
+++ packages/@dicebear/converter/src/node/core.ts
@@ -93,34 +93,41 @@ async function toBuffer(
   return buffer;
 }
 
+// https://www.iptc.org/std/photometadata/specification/IPTC-PhotoMetadata
+// https://developers.google.com/search/docs/appearance/structured-data/image-license-metadata
 function getExif(svg: string): Exif {
   const exif: Exif = {};
 
-  const description = svg.match(/<desc>(.*?)<\/desc>/s);
+  const sourceName = svg.match(/<dc:title[^>]*>(.*?)<\/dc:title>/s);
+  const sourceUrl = svg.match(/<dc:source[^>]*>(.*?)<\/dc:source>/s);
+  const creatorName = svg.match(/<dc:creator[^>]*>(.*?)<\/dc:creator>/s);
+  const licenseUrl = svg.match(/<dcterms:license[^>]*>(.*?)<\/dcterms:license>/s);
+  const copyright = svg.match(/<dc:rights[^>]*>(.*?)<\/dc:rights>/s);
 
-  const title = svg.match(/<dc:title>(.*?)<\/dc:title>/s);
-  const creator = svg.match(/<dc:creator>(.*?)<\/dc:creator>/s);
-  const source = svg.match(/<dc:source>(.*?)<\/dc:source>/s);
-  const rights = svg.match(/<dc:rights>(.*?)<\/dc:rights>/s);
-
-  if (description) {
-    exif['Description'] = description[1];
+  if (sourceName) {
+    exif['IPTC:ObjectName'] = sourceName[1];
+    exif['XMP-dc:Title'] = sourceName[1];
   }
 
-  if (title) {
-    exif['Title'] = title[1];
+  if (sourceUrl) {
+    exif['XMP-plus:LicensorURL'] = sourceUrl[1];
   }
 
-  if (creator) {
-    exif['Creator'] = creator[1];
+  if (creatorName) {
+    exif['IPTC:By-line'] = creatorName[1];
+    exif['XMP-dc:Creator'] = creatorName[1];;
+
+    exif['IPTC:Credit'] = creatorName[1];
+    exif['XMP-photoshop:Credit'] = creatorName[1];
   }
 
-  if (source) {
-    exif['Source'] = source[1];
+  if (licenseUrl) {
+    exif['XMP-xmpRights:WebStatement'] = licenseUrl[1];
   }
 
-  if (rights) {
-    exif['Rights'] = rights[1];
+  if (copyright) {
+    exif['IPTC:CopyrightNotice'] = copyright[1];
+    exif['XMP-dc:Rights'] = copyright[1];
   }
 
   return exif;
--- packages/@dicebear/core/src/utils/license.ts
+++ packages/@dicebear/core/src/utils/license.ts
@@ -1,76 +1,60 @@
 import { Style } from '../types.js';
 import * as _ from './escape.js';
 
-export function xml(style: Style<any>): string | undefined {
-  if (!requiresAttribution(style)) {
-    return undefined;
+export function xml(style: Style<any>): string {
+  const sourceName = style.meta?.source?.name;
+  const sourceUrl = style.meta?.source?.url;
+  const creatorName = style.meta?.creator?.name;
+  const licenseUrl = style.meta?.license?.url;
+  const copyright = text(style);
+
+  if (!sourceName && !creatorName && !sourceUrl && !licenseUrl && !copyright) {
+    return '';
   }
 
-  const title = style.meta?.source?.name ?? 'Unnamed';
-
-  let creator = style.meta?.creator?.name ?? 'Unknown';
-
-  if (style.meta?.creator?.url) {
-    creator += ` - ${style.meta.creator.url}`;
-  }
-
-  let license = style.meta?.license?.name ?? 'Unknown';
-
-  if (style.meta?.license?.url) {
-    license += ` - ${style.meta.license.url}`;
-  }
-
+  // https://nsteffel.github.io/dublin_core_generator/generator.html
   return (
-    `<desc>${text(style)}</desc>` +
     '<metadata' +
+    ' xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"' +
+    ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
     ' xmlns:dc="http://purl.org/dc/elements/1.1/"' +
-    ' xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">' +
+    ' xmlns:dcterms="http://purl.org/dc/terms/">' +
     '<rdf:RDF>' +
     '<rdf:Description>' +
-    `<dc:title>${_.xml(title)}</dc:title>` +
-    `<dc:creator>${_.xml(creator)}</dc:creator>` +
-    `<dc:source>${_.xml(style.meta?.source?.url ?? '')}</dc:source>` +
-    `<dc:rights>${license}</dc:rights>` +
-    `<dc:description>Remix of the original</dc:description>` +
+    (sourceName ? `<dc:title>${_.xml(sourceName)}</dc:title>` : '') +
+    (creatorName ? `<dc:creator>${_.xml(creatorName)}</dc:creator>` : '') +
+    (sourceUrl ? `<dc:source xsi:type="dcterms:URI">${_.xml(sourceUrl)}</dc:source>` : '') +
+    (licenseUrl ? `<dcterms:license xsi:type="dcterms:URI">${_.xml(licenseUrl)}</dcterms:license>` : '') +
+    (copyright ? `<dc:rights>${_.xml(copyright)}</dc:rights>` : '') +
     '</rdf:Description>' +
     '</rdf:RDF>' +
     '</metadata>'
   );
 }
 
-export function text(style: Style<any>): string | undefined {
-  if (!requiresAttribution(style)) {
-    return undefined;
-  }
-
-  let title = style.meta?.source?.name ?? 'Unnamed';
-  let creator = style.meta?.creator ?? 'Unknown';
-  let license = style.meta?.license?.name ?? 'Unknown';
+export function text(style: Style<any>): string {
+  let title = style.meta?.source?.name ? `„${style.meta?.source?.name}”` : 'Design';
+  let creator = `„${style.meta?.creator?.name ?? 'Unknown'}”`;
 
   if (style.meta?.source) {
     title += ` (${style.meta.source})`;
   }
 
-  if (style.meta?.creator?.url) {
-    creator += ` (${style.meta.creator.url})`;
+  let result = '';
+
+  if (style.meta?.license?.name !== 'MIT' && style.meta?.creator?.name !== 'DiceBear' && style.meta?.source?.name) {
+    result += 'Remix of ';
   }
 
-  if (style.meta?.license?.url) {
-    license += ` (${style.meta.license.url})`;
+  result += `${title} by ${creator}`;
+
+  if (style.meta?.license?.name) {
+    result += `, licensed under „${style.meta?.license?.name}”`;
+
+    if (style.meta?.license?.url) {
+      result += ` (${style.meta.license.url})`;
+    }
   }
 
-  if (style.meta?.license?.name === 'MIT') {
-    return `${title} by ${creator}, used under ${license}.`;
-  } else {
-    return `${title} by ${creator}, used under ${license} / Remix of the original.`;
-  }
-}
-
-function requiresAttribution(style: Style<any>): boolean {
-  // Attribution can be omitted if the designer is Florian Körner and the licence is CC0 1.0.
-  return (
-    false ===
-    (style.meta?.creator?.name === 'Florian Körner' &&
-      style.meta?.license?.name === 'CC0 1.0')
-  );
+  return result;
 }
